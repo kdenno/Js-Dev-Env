52845e787d0e9731b5aecec796c2ee0e268fdfdb
kdenno <kabuyedennis3@gmail.com>
2019-06-16 11:56:12 +0300

    add appveyor CI

--- src/index.test.js
+++ src/index.test.js
@@ -18,15 +18,18 @@ describe('index.html', () => {
         // reference the index.html file and hold it in memory
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         // define jsdom environment
-        jsdom.env(index, function(err, window) {
-            // this callback function is called after jsdom has created the virtual DOM  thats why we have access to the window object
-            const h1 = window.getElementsByTagName('h1')[0];
-            // now that we have access to our element, lets write our assertions
-            expect(h1.innerHTML).to.equal('Hello World');
-            // done(); // report the tests now and get ready for the next test
-            // close the window to free up the memory
-            window.close();
+        jsdom.env({
+            html: index,
+            done: function(err, window) {
+                // this callback function is called after jsdom has created the virtual DOM  thats why we have access to the window object
+                const h1 = window.getElementsByTagName('h1')[0];
+                // now that we have access to our element, lets write our assertions
+                expect(h1.innerHTML).to.equal('Hello World!');
+                // done(); // report the tests now and get ready for the next test
+                // close the window to free up the memory
+                window.close();
 
+            }
         });
 
     });
